b93100fbbe7397fa0083961c48cf1fe500803b56
samuel-machado <devsamuelm@gmail.com>
2020-07-15 20:14:41 -0300

    Verificação dos campos de contato

--- js/envioFormulario.js
+++ js/envioFormulario.js
@@ -1,6 +1,42 @@
+onload = () => {
+    $('#telefone').prop("required", true);
+    $('#email').prop("required", true);
+}
+
+function verificatel(){
+    let tel = $('#telefone').val()
+
+    if ($('#telefone').val().length <= 14) {
+        $('#telefone').mask('(00) 0000-00009');
+    } else {
+        $('#telefone').mask('(00) 00000-0000');
+    }
+}
+
+$(document).on("input", "#telefone", function () {
+    verificatel();
+
+    if($(this).val().trim() == ''){
+        $('#email').prop("required", true);
+    }else{
+        $('#email').prop("required", false);
+    }
+});
+$(document).on("input", "#email", function () {
+    if($(this).val().trim() == ''){
+        $('#telefone').prop("required", true);
+    }else{
+        $('#telefone').prop("required", false);
+    }
+});
+
+
+
 function postToGoogle() {
+
     var field1 = $("[name='nome']").val();
     var field2 = $("[name='telefone']").val();
+    var field21 = $("[name='email']").val();
     var field3 = $("[name='assunto']").val();
     var field4 = $("[name='mensagem']").val();
 
@@ -9,6 +45,7 @@ function postToGoogle() {
         data: {
             "entry.88412552": field1,
             "entry.2037107998": field2,
+            "entry.892208407": field21,
             "entry.1805832466": field3,
             "entry.1602261439": field4
         },
